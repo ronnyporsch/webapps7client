f53cb07c1d807bd4020d8adb6c77c57d9fd9d9b7
Pramit Kumar Bhaduri <pramitkumarbhaduri@gmail.com>
2023-06-25 18:58:57 +0200

    made changes to create and delete

---
+++ src/1-MinimalApp/1-MinimalApp/public/js/v/deleteEvent.mjs
@@ -0,0 +1,38 @@
+/**
+ * @author Pramit Kumar Bhaduri
+ * @author Florian Rühs
+ */
+
+import Event from "../m/Event.mjs";
+
+const eventRecords = await Event.retrieveAll();
+
+/***************************************************************
+ Declare variables for accessing UI elements
+ ***************************************************************/
+const formEl = document.forms["Event"],
+  deleteButton = formEl["commit"],
+  selectEventEl = formEl["selectEvent"];
+
+/***************************************************************
+ Set up select element
+ ***************************************************************/
+for (const eventRec of eventRecords) {
+  const optionEl = document.createElement("option");
+  optionEl.text = eventRec.name;
+  optionEl.value = eventRec.eventID;
+  selectEventEl.add( optionEl, null);
+}
+
+/******************************************************************
+ Add event listeners for the delete/submit button
+ ******************************************************************/
+// set an event handler for the delete button
+deleteButton.addEventListener("click", async function () {
+  const eventID = selectEventEl.value;
+  if (!eventID) return;
+  if (confirm("Do you really want to delete this event record?")) {
+    await Event.destroy( eventID);
+    selectEventEl.remove( selectEventEl.selectedIndex);
+  }
+});
